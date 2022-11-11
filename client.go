e3dd76a694fbe10bae13a38379a19209a9527b37
tay3223 <tay3223@qq.com>
2022-11-11 22:38:16 +0800

    init

--- client.go
+++ client.go
@@ -1,4 +1,4 @@
-package main
+package requests
 
 import (
 	"bytes"
@@ -11,37 +11,37 @@ import (
 	"strings"
 )
 
-func main() {
-
-	//测试Get请求
-	a1 := Args{
-		Headers: map[string]interface{}{
-			"Content-Type": "application/json",
-		},
-		Params: map[string]interface{}{
-			"id": 12131,
-		},
-	}
-	client1 := Client{}
-	result1 := client1.Get("http://cmdb02.zmops.cc/api/v1/applications/lite/", a1)
-	fmt.Println(string(result1))
-
-	//测试Post请求
-	a2 := Args{
-		Headers: map[string]interface{}{
-			"Content-Type": "application/json",
-		},
-		Body: map[string]interface{}{
-			"name":    "zhangsan",
-			"age":     18,
-			"address": "shanghai",
-		},
-	}
-
-	client2 := Client{}
-	result2 := client2.Post("http://127.0.0.1:4523/m1/1911173-0-default/v1/student", a2)
-	fmt.Println(string(result2))
-}
+//func main() {
+//
+//	//测试Get请求
+//	a1 := Args{
+//		Headers: map[string]interface{}{
+//			"Content-Type": "application/json",
+//		},
+//		Params: map[string]interface{}{
+//			"id": 12131,
+//		},
+//	}
+//	client1 := Client{}
+//	result1 := client1.Get("http://cmdb02.zmops.cc/api/v1/applications/lite/", a1)
+//	fmt.Println(string(result1))
+//
+//	//测试Post请求
+//	a2 := Args{
+//		Headers: map[string]interface{}{
+//			"Content-Type": "application/json",
+//		},
+//		Body: map[string]interface{}{
+//			"name":    "zhangsan",
+//			"age":     18,
+//			"address": "shanghai",
+//		},
+//	}
+//
+//	client2 := Client{}
+//	result2 := client2.Post("http://127.0.0.1:4523/m1/1911173-0-default/v1/student", a2)
+//	fmt.Println(string(result2))
+//}
 
 type Args struct {
 	Headers map[string]interface{}
